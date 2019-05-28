eb269dc9ebc90fcb8f80dcc33af8d38969b70faf
Richard Franzese <thechosenrick@gmail.com>
2019-05-28 00:39:31 -0400

    Cleaning HTML/CSS

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,26 +4,41 @@ import ScrollMagic from 'ScrollMagic/ScrollMagic.min.js';
 import TimelineMax from 'TimelineMax/TimelineMax.min.js';
 
 
-// DOM is ready
+// wait for document ready
 document.addEventListener('DOMContentLoaded', function(){
-  // let wrapper = document.querySelector('.hero');
-  // if (wrapper) {
-  //   let controller = new ScrollMagic.Controller();
-  //   // define movement of panels
-  //   var wipeAnimation = new TimelineMax()
-  //     .fromTo('.hero section.photo', 1, {x: '-100%'}, {x: '0%', ease: Linear.easeNone})  // in from left
-  //     .fromTo('.hero section.blueprint',    1, {x:  '100%'}, {x: '0%', ease: Linear.easeNone})  // in from right
-  //     .fromTo('.hero section.zoom', 1, {y: '-100%'}, {y: '0%', ease: Linear.easeNone}); // in from top
-  //
-  //   // create scene to pin and link animation
-  //   new ScrollMagic.Scene({
-  //     triggerElement: '.hero',
-  //     triggerHook: 'onLeave',
-  //     duration: '300%'
-  //   })
-  //   .setPin('.hero')
-  //   .setTween(wipeAnimation)
-  //   .addIndicators() // add indicators (requires plugin)
-  //   .addTo(controller);
-  // }
+    // // init
+    // var controller = new ScrollMagic.Controller();
+    //
+    // // define movement of panels
+    // var wipeAnimation = new TimelineMax()
+    //   .fromTo("section.panel.turqoise", 1, {
+    //     x: "-100%"
+    //   }, {
+    //     x: "0%",
+    //     ease: Linear.easeNone
+    //   }) // in from left
+    //   .fromTo("section.panel.green", 1, {
+    //     x: "100%"
+    //   }, {
+    //     x: "0%",
+    //     ease: Linear.easeNone
+    //   }) // in from right
+    //   .fromTo("section.panel.bordeaux", 1, {
+    //     y: "-100%"
+    //   }, {
+    //     y: "0%",
+    //     ease: Linear.easeNone
+    //   }); // in from top
+    //
+    // // create scene to pin and link animation
+    // var scene = new ScrollMagic.Scene({
+    //   triggerElement: "#pinContainer",
+    //   triggerHook: "onLeave",
+    //   duration: "300%"
+    // });
+    // // debugger;
+    // scene.setPin("#pinContainer");
+    // scene.setTween(wipeAnimation);
+    // scene.addIndicators(); // add indicators (requires plugin)
+    // scene.addTo(controller);
 });
